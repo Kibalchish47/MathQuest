f45ba75861114227910e08358edc473ff162b55e
Kibalchish47 <kibalchishthecoder@gmail.com>
2024-07-09 21:18:49 +0300

    Improved documentation and readability

--- dialogue/dialogue.py
+++ dialogue/dialogue.py
@@ -2,21 +2,27 @@ mathematician_filenames = {
     "Fibonacci": "dialogue/fibonacci_dialogue.txt",  
 }
 
-def mathematician_filename_find(dialogue_name: str) -> str:
-    # Helper function that takes care of fetching the image of a given mathematician
-    # Input(s): Name of the mathematician (dialogue_name)
-    # Output(s): Image of the mathematician
-    return mathematician_filenames.get(dialogue_name, "Mathematician's image not found")
+def mathematician_filepath_find(mathematician_name: str) -> str:
+    # f: Helper function that takes care of fetching the path to a given mathematician's image
+    # Input(s): Name of the mathematician (mathematician_name)
+    # Output(s): Path to a given mathematician's image
+    return mathematician_filenames.get(mathematician_name, "Mathematician's image not found")
 
-def read_dialogue_from_file(filename: str) -> str:
+def read_image_from_file(filepath: str) -> str:
+    # f: ...
+    # Input(s): Path to a mathematician's image
+    # Output(s): Mathematician's image (in dialogue) 
     try:
-        with open(filename, "r") as file:
+        with open(filepath, "r") as file:
             return file.read()
     except FileNotFoundError:
         return "File not found"
 
-def mathematician_image_show(character_image: str) -> None: 
+def dialogue_image_show(character_image: str) -> None:
+    # f: Prints out the mathematician's image (to be improved)
+    # Input(s): Mathematician's image
+    # Output(s): None 
     print(character_image)
 
 # Testing call for the function
-mathematician_image_show(read_dialogue_from_file(mathematician_filename_find("Fibonacci")))
+dialogue_image_show(read_image_from_file(mathematician_filepath_find("Fibonacci")))
